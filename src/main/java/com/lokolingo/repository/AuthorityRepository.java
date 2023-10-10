eca6c3e3e5dcf32d7513a1993b9a339c882f2ff1
Alex Mungai <murungarumungai@gmail.com>
2023-10-10 22:49:49 +0300

    Initial version of Lokolingo generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/lokolingo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.lokolingo.repository;
+
+import com.lokolingo.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/lokolingo/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.lokolingo;
+
+import com.lokolingo.LokolingoApp;
+import com.lokolingo.config.AsyncSyncConfiguration;
+import com.lokolingo.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { LokolingoApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
